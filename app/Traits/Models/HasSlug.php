5efb3f94c946b00f4d0f9fad36e78ec3c184e37c
Камиль <94285493+kamil19862307@users.noreply.github.com>
2023-05-19 19:30:36 +0300

    unique slug from title

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -6,19 +6,59 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug
 {
-    protected static function bootHasSlug()
+    protected static function bootHasSlug(): void
     {
         static::creating(function (Model $item)
         {
-            $item->slug = $item->slug
-                ?? str($item->{self::slugFrom()})
-                    ->append(time())
-                    ->slug();
+            $item->makeSlug();
         });
     }
 
-    public static function slugFrom(): string
+    protected function makeSlug(): void
+    {
+        if (!$this->{$this->slugColumn()}){
+            $slug = $this->slugUnique(
+                str($this->{$this->slugFrom()})
+                    ->slug()
+                    ->value()
+            );
+        }
+
+        $this->{$this->slugColumn()} = $slug;
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    protected function slugFrom(): string
     {
         return 'title';
+    }
+
+    private function slugUnique(string $slug):string
+    {
+        $originalSlug = $slug;
+        $i = 1;
+
+        while ($this->isSlugExists($slug)){
+            $i++;
+
+            $slug = $originalSlug. '-' .$i;
+        }
+
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            // Не беру за основу текущюю запись (если появится update, пока только creating)
+            ->where($this->getKeyName(), '!=', $this->getKey())
+            ->withoutGlobalScopes();
+
+        return $query->exists();
     }
 }
